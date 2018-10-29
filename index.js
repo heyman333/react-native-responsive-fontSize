b60f08c1391f8dfb01d4d672b348844d88292b15
jamesmcn1 <james@pensionbee.com>
2018-10-29 11:40:02 +0100

    adds fontsize fn

--- index.js
+++ index.js
@@ -3,11 +3,22 @@ import { Platform, StatusBar, Dimensions } from "react-native"
 
 const { height, width } = Dimensions.get("window")
 
-export default function RF(percent) {
+export default function RFPercentage(percent) {
   const deviceHeight = isIphoneX()
     ? height * 0.9
     : Platform.OS === "android" ? height - StatusBar.currentHeight : height
 
   const heightPercent = percent * deviceHeight / 100
+  return Math.round(heightPercent)
+}
+
+export default function RFValue(fontSize) {
+  const deviceHeight = isIphoneX()
+    ? height * 0.9
+    : Platform.OS === "android" ? height - StatusBar.currentHeight : height
+
+// guideline height for standard 5" device screen
+  const standardScreenHeight = 680
+  const heightPercent = fontSize * height / standardHeight
   return Math.round(heightPercent)
 }
