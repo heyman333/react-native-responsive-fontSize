968a094559fc9869e7cdf01bab40b4ec0a909490
jamesmcn1 <james@pensionbee.com>
2018-10-29 11:43:46 +0100

    slight refactor and fixes typos

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 import { isIphoneX } from "react-native-iphone-x-helper"
 import { Platform, StatusBar, Dimensions } from "react-native"
 
-const { height, width } = Dimensions.get("window")
-
 export default function RFPercentage(percent) {
-  const deviceHeight = isIphoneX()
-    ? height * 0.9
-    : Platform.OS === "android" ? height - StatusBar.currentHeight : height
-
   const heightPercent = percent * deviceHeight / 100
   return Math.round(heightPercent)
 }
 
 export default function RFValue(fontSize) {
-  const deviceHeight = isIphoneX()
-    ? height * 0.9
-    : Platform.OS === "android" ? height - StatusBar.currentHeight : height
-
 // guideline height for standard 5" device screen
   const standardScreenHeight = 680
-  const heightPercent = fontSize * height / standardHeight
+  const heightPercent = fontSize * deviceHeight / standardScreenHeight
   return Math.round(heightPercent)
 }
+
+// helpers
+const { height, width } = Dimensions.get("window")
+
+const deviceHeight = isIphoneX()
+? height * 0.9
+: Platform.OS === "android" ? height - StatusBar.currentHeight : height
