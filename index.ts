3cc49879e8a4a6a923dc514a4af60438cb5ee608
Donald Koo <donaldkoo72@gmail.com>
2018-12-05 15:46:06 -0500

    Increasing font size on landscape
    I was using this and had an issue where font sizes in landscape were appearing extremely small because it was using device height. I made this change to make things easier to read if the app begins in landscape orientation.

--- index.ts
+++ index.ts
@@ -1,15 +1,27 @@
 import { isIphoneX } from "react-native-iphone-x-helper";
 import { Platform, StatusBar, Dimensions } from "react-native";
 
-const { height } = Dimensions.get("window");
+const { height, width } = Dimensions.get("window");
 
 export default function RF(percent: number) {
-  const deviceHeight = isIphoneX()
-    ? height - 78 // iPhone X style SafeAreaView size in portrait
-    : Platform.OS === "android"
-    ? height - StatusBar.currentHeight!
-    : height;
+  if(height > width){
+    const deviceHeight = isIphoneX()
+      ? height - 78 // iPhone X style SafeAreaView size in portrait
+      : Platform.OS === "android"
+      ? height - StatusBar.currentHeight!
+      : height;
 
-  const heightPercent = (percent * deviceHeight) / 100;
-  return Math.round(heightPercent);
+    const heightPercent = (percent * deviceHeight) / 100;
+    return Math.round(heightPercent);
+  }
+  else{
+    const deviceWidth = isIphoneX()
+      ? width - 78
+      : Platform.OS === 'android'
+      ? width - StatusBar.currentWidth!
+      : width;
+    
+    const widthPercent = percent * deviceWidth / 100;
+    return Math.round(widthPercent);
+  }
 }
