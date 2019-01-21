6a7ddb710e28f58af5421bc160b23f6988dfdb77
Adam Sajko <pronasa@live.com>
2019-01-21 16:31:29 +0100

    typings separated into a different file

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ import { Platform, StatusBar, Dimensions } from "react-native";
 
 const { height } = Dimensions.get("window");
 
-export default function RF(percent: number) {
+export default function RF(percent) {
   const deviceHeight = isIphoneX()
     ? height - 78 // iPhone X style SafeAreaView size in portrait
     : Platform.OS === "android"
-    ? height - StatusBar.currentHeight!
+    ? height - StatusBar.currentHeight
     : height;
 
   const heightPercent = (percent * deviceHeight) / 100;
