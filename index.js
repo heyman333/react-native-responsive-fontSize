cc8c67e132a179e9c1b431f2cb290305211ed633
Yeongsu Han <amazingmobdev@gmail.com>
2020-02-05 01:57:37 +0900

    bump versoin string to 0.4.3 and fix landscape offset (#21)

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import { Platform, StatusBar, Dimensions } from "react-native";
 
 const { height, width } = Dimensions.get("window");
 const standardLength = width > height ? width : height;
+const offset =
+  width > height ? 0 : Platform.OS === "ios" ? 78 : StatusBar.currentHeight; // iPhone X style SafeAreaView size in portrait
 
-const deviceHeight = isIphoneX()
-  ? standardLength - 78 // iPhone X style SafeAreaView size in portrait
-  : Platform.OS === "android"
-  ? standardLength - StatusBar.currentHeight
-  : standardLength;
+const deviceHeight =
+  isIphoneX() || Platform.OS === "android"
+    ? standardLength - offset
+    : standardLength;
 
 export function RFPercentage(percent) {
   const heightPercent = (percent * deviceHeight) / 100;
