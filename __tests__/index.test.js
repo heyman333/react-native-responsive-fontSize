c3ec4b45fcd55ea06e862188471f9a167825c441
youngsoohan <amazingmobdev@gmail.com>
2021-07-17 20:01:27 +0900

    feat: testcode

---
+++ __tests__/index.test.js
@@ -0,0 +1,82 @@
+import { RFValue, RFPercentage } from "../index";
+import { isIphoneX } from "react-native-iphone-x-helper";
+import { Dimensions } from "react-native";
+
+const rnLib = "../node_modules/react-native/Libraries";
+
+jest.mock("../node_modules/react-native/Libraries/Utilities/Dimensions");
+jest.mock("react-native-iphone-x-helper");
+describe("react-native-responsive-fontsize", () => {
+  jest.mock(rnLib + "/Components/StatusBar/StatusBar", () => ({
+    currentHeight: 100,
+  }));
+
+  test("when iPhoneX is false RFValue", () => {
+    Dimensions.get.mockImplementationOnce(
+      jest.fn(() => ({ width: 100, height: 680 }))
+    );
+    isIphoneX.mockReturnValue(false);
+    expect(RFValue(20)).toBe(20);
+  });
+
+  test("when iPhoneX is false RFPercentage", () => {
+    Dimensions.get.mockImplementationOnce(
+      jest.fn(() => ({ width: 100, height: 680 }))
+    );
+    isIphoneX.mockReturnValue(false);
+    expect(RFPercentage(3)).toBe(20);
+  });
+
+  test("when iPhoneX is true RFValue", () => {
+    Dimensions.get.mockImplementationOnce(
+      jest.fn(() => ({ width: 100, height: 680 }))
+    );
+    isIphoneX.mockReturnValue(true);
+    expect(RFValue(20)).toBe(18);
+  });
+
+  test("when iPhoneX is true RFPercentage", () => {
+    Dimensions.get.mockImplementationOnce(
+      jest.fn(() => ({ width: 100, height: 680 }))
+    );
+    isIphoneX.mockReturnValue(true);
+    expect(RFPercentage(3)).toBe(18);
+  });
+
+  test("when width is bigger than height RFPercentage", () => {
+    Dimensions.get.mockImplementationOnce(
+      jest.fn(() => ({ width: 680, height: 100 }))
+    );
+    isIphoneX.mockReturnValue(true);
+    expect(RFPercentage(3)).toBe(20);
+  });
+
+  test("when width is bigger than RFValue", () => {
+    Dimensions.get.mockImplementationOnce(
+      jest.fn(() => ({ width: 680, height: 100 }))
+    );
+    isIphoneX.mockReturnValue(true);
+    expect(RFValue(20)).toBe(20);
+  });
+
+  test("when Platform is android RFPercentage", () => {
+    jest.mock("react-native/Libraries/Utilities/Platform", () => ({
+      OS: "android",
+      select: () => null,
+    }));
+
+    Dimensions.get.mockImplementationOnce(
+      jest.fn(() => ({ width: 100, height: 680 }))
+    );
+    isIphoneX.mockReturnValue(true);
+    expect(RFPercentage(3)).toBe(17);
+  });
+
+  test("when Platform is android RFValue", () => {
+    Dimensions.get.mockImplementationOnce(
+      jest.fn(() => ({ width: 100, height: 680 }))
+    );
+    isIphoneX.mockReturnValue(true);
+    expect(RFValue(20)).toBe(17);
+  });
+});
--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 import { isIphoneX } from "react-native-iphone-x-helper";
 import { Platform, StatusBar, Dimensions } from "react-native";
 
-const { height, width } = Dimensions.get("window");
-const standardLength = width > height ? width : height;
-const offset =
-  width > height ? 0 : Platform.OS === "ios" ? 78 : StatusBar.currentHeight; // iPhone X style SafeAreaView size in portrait
-
-const deviceHeight =
-  isIphoneX() || Platform.OS === "android"
-    ? standardLength - offset
-    : standardLength;
-
 export function RFPercentage(percent) {
+  const { height, width } = Dimensions.get("window");
+  const standardLength = width > height ? width : height;
+  const offset =
+    width > height ? 0 : Platform.OS === "ios" ? 78 : StatusBar.currentHeight; // iPhone X style SafeAreaView size in portrait
+
+  const deviceHeight =
+    isIphoneX() || Platform.OS === "android"
+      ? standardLength - offset
+      : standardLength;
+
   const heightPercent = (percent * deviceHeight) / 100;
   return Math.round(heightPercent);
 }
 
 // guideline height for standard 5" device screen is 680
 export function RFValue(fontSize, standardScreenHeight = 680) {
+  const { height, width } = Dimensions.get("window");
+  const standardLength = width > height ? width : height;
+  const offset =
+    width > height ? 0 : Platform.OS === "ios" ? 78 : StatusBar.currentHeight; // iPhone X style SafeAreaView size in portrait
+
+  const deviceHeight =
+    isIphoneX() || Platform.OS === "android"
+      ? standardLength - offset
+      : standardLength;
+
   const heightPercent = (fontSize * deviceHeight) / standardScreenHeight;
   return Math.round(heightPercent);
 }
