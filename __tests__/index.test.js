fc6fe977368afad885eb542132292b50429aa8f6
youngsoohan <amazingmobdev@gmail.com>
2021-07-17 20:29:49 +0900

    bump version

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,12 +2,10 @@ import { RFValue, RFPercentage } from "../index";
 import { isIphoneX } from "react-native-iphone-x-helper";
 import { Dimensions } from "react-native";
 
-const rnLib = "../node_modules/react-native/Libraries";
-
-jest.mock("../node_modules/react-native/Libraries/Utilities/Dimensions");
+jest.mock("react-native/Libraries/Utilities/Dimensions");
 jest.mock("react-native-iphone-x-helper");
 describe("react-native-responsive-fontsize", () => {
-  jest.mock(rnLib + "/Components/StatusBar/StatusBar", () => ({
+  jest.mock("react-native/Libraries/Components/StatusBar/StatusBar", () => ({
     currentHeight: 100,
   }));
 
@@ -43,6 +41,14 @@ describe("react-native-responsive-fontsize", () => {
     expect(RFPercentage(3)).toBe(18);
   });
 
+  test("when width is bigger than height RFValue", () => {
+    Dimensions.get.mockImplementationOnce(
+      jest.fn(() => ({ width: 680, height: 100 }))
+    );
+    isIphoneX.mockReturnValue(true);
+    expect(RFValue(20)).toBe(20);
+  });
+
   test("when width is bigger than height RFPercentage", () => {
     Dimensions.get.mockImplementationOnce(
       jest.fn(() => ({ width: 680, height: 100 }))
@@ -51,32 +57,23 @@ describe("react-native-responsive-fontsize", () => {
     expect(RFPercentage(3)).toBe(20);
   });
 
-  test("when width is bigger than RFValue", () => {
-    Dimensions.get.mockImplementationOnce(
-      jest.fn(() => ({ width: 680, height: 100 }))
-    );
-    isIphoneX.mockReturnValue(true);
-    expect(RFValue(20)).toBe(20);
-  });
-
-  test("when Platform is android RFPercentage", () => {
+  test("when Platform is android RFValue", () => {
     jest.mock("react-native/Libraries/Utilities/Platform", () => ({
       OS: "android",
       select: () => null,
     }));
-
-    Dimensions.get.mockImplementationOnce(
-      jest.fn(() => ({ width: 100, height: 680 }))
-    );
-    isIphoneX.mockReturnValue(true);
-    expect(RFPercentage(3)).toBe(17);
-  });
-
-  test("when Platform is android RFValue", () => {
     Dimensions.get.mockImplementationOnce(
       jest.fn(() => ({ width: 100, height: 680 }))
     );
     isIphoneX.mockReturnValue(true);
     expect(RFValue(20)).toBe(17);
+  });
+
+  test("when Platform is android RFPercentage", () => {
+    Dimensions.get.mockImplementationOnce(
+      jest.fn(() => ({ width: 100, height: 680 }))
+    );
+    isIphoneX.mockReturnValue(true);
+    expect(RFPercentage(3)).toBe(17);
   });
 });
